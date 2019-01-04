eb0d93509619e8dbdd1f2419310d89be54f1d966
Ben Stidham <benjamindstidham@gmail.com>
2019-01-04 13:54:45 -0600

    continued LinkedList practice

---
+++ linkedList.js
@@ -0,0 +1,65 @@
+//Linked List
+
+//Declare a LinkedList Constructor
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+//Declare a Node
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+//Add 'addToHead' Method to the LinkedList
+LinkedList.prototype.addToHead = function(value) {
+  //New Node is created with the value passed in from parameter.
+  //The 'next' property is passed in 'this.head' to point the new Node in the direction of the prexisting Node. If there are no Nodes in the LinkedList, 'this.head' will be null'
+  let newNode = new Node(value, this.head, null);
+  //"If a node exists..." the prexisting head's Node will point its prev property to the 'newNode'
+  //Otherwise, if no Node exists in the LinkedList, 'this.tail' becomes the newNode.
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+  //Whether there are Nodes existing in the LinkedList or not, 'this.head' will become the newNode.
+  this.head = newNode;
+};
+
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode;
+};
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) return null;
+  let val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) this.tail.next = null;
+  else this.head = null;
+  return val;
+};
+
+var LL = new LinkedList();
+
+LL.addToHead(1000);
+LL.addToHead(2000);
+LL.addToTail(3000);
+
+LL.removeHead();
+LL.removeTail();
+LL.addToHead(123);
+LL.addToHead(70);
+LL.addToHead("hello");
+LL.addToTail(19);
+LL.addToTail("world");
+LL.addToTail(20);
+
+console.log(LL.search("hello"));
