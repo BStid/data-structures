c3c33182bdda325e1a3b5b29eb11f009fdab148a
Ben Stidham <benjamindstidham@gmail.com>
2019-01-07 12:09:09 -0600

    hashTable complete

--- hashTable.js
+++ hashTable.js
@@ -1,7 +1,20 @@
+//HashTable
+//0(1)
+
+/* 
+- Constant time insertion & lookup
+- Data doesn't store reference to other pieces of data in the data structre.
+
+Practical Uses:
+    - Email provider storing addresses
+    -Users of an application
+*/
+
 function HashTable(size) {
   //defines where the data will be stored
   //"Make a new array of whatever size and assign it to this.buckets"
   this.buckets = Array(size);
+  //number of buckets in the array;
   this.numBuckets = this.buckets.length;
 }
 
@@ -24,6 +37,71 @@ HashTable.prototype.hash = function(key) {
   return bucket;
 };
 
-let myHT = new HashTable(30);
+HashTable.prototype.insert = function(key, value) {
+  //hash the key to determine the index of the bucket
+  let index = this.hash(key);
+  //if the index of the bucket is empty, create a new hashNode
+  if (!this.buckets[index]) this.buckets[index] = new HashNode(key, value);
+  else if (this.buckets[index].key === key) {
+    this.buckets[index].value = value;
+  }
+  //otherwise, if the bucket has a node, travel to the end of the chain and create a new HashNode
+  else {
+    let currentNode = this.buckets[index];
+    while (currentNode.next) {
+      //if the key on the next Node is equal to the key we pass in...
+      if (currentNode.next.key === key) {
+        //set the next Node's value to the value we pass in
+        currentNode.next.value = value;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+    currentNode.next = new HashNode(key, value);
+  }
+};
 
-console.log(myHT.hash("Benjamin"));
+HashTable.prototype.get = function(key) {
+  //hash the key into an index to find the correct bucket
+  let index = this.hash(key);
+  //if nothing in the bucket... return null
+  if (!this.buckets[index]) return null;
+  //otherwise, travel through all the nodes in that bucket with the while loop.
+  else {
+    let currentNode = this.buckets[index];
+    while (currentNode) {
+      //if the key parameter matches any of the keys in the currentNode, return the value of that currentNode
+      if (currentNode.key === key) return currentNode.value;
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+};
+
+HashTable.prototype.retrieveAll = function() {
+  //define empty array to store all nodes
+  let allNodes = [];
+  //loop through the entire bucket
+  for (let i = 0; i < this.numBuckets; i++) {
+    let currentNode = this.buckets[i];
+    while (currentNode) {
+      allNodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+  }
+  return allNodes;
+};
+const myHT = new HashTable(30);
+
+// console.log(myHT.hash('Benjamin'));
+myHT.insert("Ben", "ben@benstidham.com");
+myHT.insert("Dean", "dean@gmail.com");
+myHT.insert("Megan", "megan@gmail.com");
+myHT.insert("Dane", "dane@yahoo.com");
+myHT.insert("Dean", "deanmachine@gmail.com");
+myHT.insert("Megan", "megansmith@gmail.com");
+myHT.insert("Dane", "daneboy@gmail.com");
+myHT.insert("Joe", "joey@google.com");
+myHT.insert("Samantha", "sameow@zoo.com");
+
+console.log(myHT.retrieveAll());
