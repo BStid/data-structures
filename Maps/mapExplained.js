cd0326eace79039a44cb1d5be9b8447dfe79ed37
Ben Stidham <benjamindstidham@gmail.com>
2019-04-17 00:02:04 -0500

    added options to map

--- Maps/mapExplained.js
+++ Maps/mapExplained.js
@@ -11,5 +11,11 @@ myMapName.set(keyString, "Value Associated with this key...");
 myMapName.set(keyObj, "value associated with the obj");
 myMapName.set(keyFunc, "value associated with the function");
 
+//function options...
+// .set(key,value)
+// .get(key)
+// .foreach(callBackFunction)
+// .has(key)
+
 // console.log(myMapName.get(keyFunc));
 console.log(myMapName.get("KeyThatIsAString"));
